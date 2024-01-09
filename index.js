a9c02f18ee5a3feed6944cfb2ef469381e56ed71
Abhigupta13 <abhishekkumargupta22110@gmail.com>
2024-01-09 11:49:41 +0530

    User, Cart, Orders added

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const app = express();
  const productsRouter = require('./routes/Products');
 const categoriesRouter = require('./routes/Categories');
 const brandsRouter = require('./routes/Brands');
+const authRouter = require('./routes/Auth');
+const usersRouter = require('./routes/Users');
+const cartRouter = require('./routes/Cart');
+const ordersRouter = require('./routes/Order');
 const cors = require('cors');
 const connectDB  = require('./config/db');
-const PORT =process.env.PORT || 8080
+const {PORT} = require('./config/serverConfig');
 
 //middlewares
 
@@ -16,6 +20,10 @@ app.use(express.json()); // to parse req.body
 app.use('/products', productsRouter.router);
 app.use('/categories', categoriesRouter.router)
 app.use('/brands', brandsRouter.router)
+app.use('/users', usersRouter.router)
+app.use('/auth', authRouter.router)
+app.use('/cart', cartRouter.router)
+app.use('/orders', ordersRouter.router)
 
 
 
@@ -23,7 +31,7 @@ app.get('/',(req, res)=>{
     res.json({status:'success'})
 })
 
-
+// https://www.youtube.com/watch?v=LH-S5v-D3hA
 app.listen(PORT, async()=>{
     connectDB();
     console.log('Server started on PORT',PORT)
---
+++ model/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true, default:'user' },
+  addresses: { type: [mongoose.Schema.Types.Mixed] }, 
+  // TODO:  We can make a separate Schema for this
+  name: { type: String },
+  orders: { type: [mongoose.Schema.Types.Mixed] }
+});
+
+const virtual = userSchema.virtual('id');
+virtual.get(function () {
+  return this._id;
+});
+userSchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+  },
+});
+
+exports.User = mongoose.model('User', userSchema);
