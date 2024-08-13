f4e8ea6325d7cfb212fa16fb6c5a60a2eb357418
abhishek-stratex <abhishek.gupta@stratexcs.com>
2024-08-14 00:48:53 +0530

    error handling

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -2,9 +2,9 @@ const { Product } = require('../model/Product');
 
 exports.createProduct = async (req, res) => {
   // this product we have to get from API body
+  try {
   const product = new Product(req.body);
   product.discountPrice = Math.round(product.price*(1-product.discountPercentage/100))
-  try {
     const doc = await product.save();
     res.status(201).json(doc);
   } catch (err) {
